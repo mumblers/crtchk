782e866e2b43462051d6b642bada8255ed351b83
Sinius15 <sinius15@gmail.com>
2016-04-16 23:25:24 +0200

    Adding basic service layer

---
+++ service/index.php
@@ -0,0 +1,8 @@
+<?php
+$orignal_parse = parse_url($_GET['url'], PHP_URL_HOST);
+$get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+$read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+$cert = stream_context_get_params($read);
+$certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+
+echo json_encode($certinfo);
