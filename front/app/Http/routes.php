a5d97b33a602aebd9ff8280b762c001fa81389e5
pijke <me.accounts+gitlabacro@runbox.com>
2016-04-17 10:40:10 +0200

    Added login functionallity and started on the dashboard theme

--- front/app/Http/routes.php
+++ front/app/Http/routes.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('front.home');
+});
+
+Route::auth();
+
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::get('/', 'DashboardController@showIndex');
 });
