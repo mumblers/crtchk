deacfac6189b3da4574a8ae03ff4a0fe11356f2c
Sinius15 <sinius15@gmail.com>
2016-04-23 20:56:03 +0200

    Setting up structure for domain crud

--- front/app/Http/Controllers/DomainController.php
+++ front/app/Http/Controllers/DomainController.php
@@ -2,10 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+
 class DomainController extends BaseController {
 
-	function getPage(){
-		return view('dashboard.domains');
+	/**
+	 * Show a list of all domains of the current logged in user.
+	 * @return $this
+	 */
+	function getAll(){
+		$user = Auth::user();
+
+		$domains = $user->domains;
+
+		return view('dashboard.domains')
+			->with("domains" , $domains);
 	}
+
+	/**
+	 * Get the information of a single Domain.
+	 * @return $this
+	 */
+	function getView(){
+
+	}
+
+	/**
+	 * Get the page where you can edit a Domain.
+	 * @return $this
+	 */
+	function getEdit(){
+
+	}
+
+	/**
+	 * Get the page where you can delete a Domain.
+	 * @return $this
+	 */
+	function getDelete(){
+
+	}
+
+	/**
+	 * Handle an edit action of a Domain.
+	 * @return $this
+	 */
+	function postEdit(){
+
+	}
+
+	/**
+	 * Handle a del
+	 * @return $this
+	 */
+	function postDelete(){
+
+	}
+
 
 }
