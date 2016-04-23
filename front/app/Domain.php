336ba8f0d8a3bdade47f56fd453e019a65757904
Sinius15 <sinius15@gmail.com>
2016-04-23 19:58:27 +0200

    Adding documentation for Models

--- front/app/Domain.php
+++ front/app/Domain.php
@@ -5,6 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Domain
+ *
+ * @property string $id
+ * @property string $user_id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $deleted_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Domain whereDeletedAt($value)
+ * @mixin \Eloquent
+ */
 class Domain extends Model {
 	use SoftDeletes, UuidForKey;
 
@@ -14,17 +32,12 @@ class Domain extends Model {
 
 	protected $fillable = ['name'];
 
-	/**
-	 * @return \Illuminate\Database\Query\Builder
-	 */
-	public function domains(){
-		return $this->hasMany('App\Domain', 'user_id', 'id');
-	}
+
 
 	/**
 	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
 	 */
-	public function leerling(){
+	public function user(){
 		return $this->belongsTo('App\User', 'user_id', 'id');
 	}
 }
--- front/app/User.php
+++ front/app/User.php
@@ -7,6 +7,34 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Bican\Roles\Traits\HasRoleAndPermission;
 use Bican\Roles\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
 
+/**
+ * App\User
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $api_key
+ * @property string $settings
+ * @property string $deleted_at
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Domain[] $domains
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Bican\Roles\Models\Role[] $roles
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Bican\Roles\Models\Permission[] $userPermissions
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereApiKey($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereSettings($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements HasRoleAndPermissionContract {
     use HasRoleAndPermission, SoftDeletes, UuidForKey;
     
